2d337fa16200a4273670f1fda91d0ccc5b51ef3e
Will Chen <wfchen1990@gmail.com>
2023-05-31 17:53:10 -0600

    Add methods

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,16 @@ student_ages = [27, 32, 45, 33]
 weights = [21.4, 43.2, 78.1, 50.5]
 booleans = [true, true, false, false]
 
+#remove last element
+dog_names.pop
+#sorry Harold
+
+#remove first element
+student_ages.shift
+
+#add an element to the end of the array
+weights.push(45.3)
+
+#add an element to the front of the array
+booleans.unshift(false)
+
