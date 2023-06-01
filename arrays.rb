dc7ea93a82f27f6becf8e7cf48aa74e96fdb8460
Will Chen <wfchen1990@gmail.com>
2023-05-31 18:05:16 -0600

    Add reverse method

--- arrays.rb
+++ arrays.rb
@@ -3,18 +3,22 @@ student_ages = [27, 32, 45, 33]
 weights = [21.4, 43.2, 78.1, 50.5]
 booleans = [true, true, false, false]
 
-#remove last element
+#remove the last name from the list
 dog_names.pop
 #sorry Harold
 
-#remove first element
+#remove the first age from the list
 student_ages.shift
 
-#add an element to the end of the array
+#add a weight to the end of the list
 weights.push(45.3)
 
-#add an element to the front of the array
+#add an value to the front of the list
 booleans.unshift(false)
 
 #Pipa is the second element and in index position 1 in the array dog_names
 dog_names[1]
+
+#the reverse method flips the order of the elements within the array.  the example below will go from ascending to descending
+numbers = [1, 2, 3, 4]
+numbers.reverse
